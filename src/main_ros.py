f9ff84483503e97ed662e8896d7788ed2e69a435
Joram <1075865@hr.nl>
2026-06-09 14:17:17 +0200

    Set frequency and set correct camera for docker file

--- src/main_ros.py
+++ src/main_ros.py
@@ -340,7 +340,7 @@ def main():
                     break
 
             rclpy.spin_once(node, timeout_sec=0.0)
-            time.sleep(0.05)
+            time.sleep(0.2)
 
     finally:
         cap.release()
